4b4ee3c3262683b08b721becad7a02a2152d77f1
Hassan Olakunle <kunlex_042003@yahoo.com>
2020-03-23 19:01:28 +0100

    scritp file

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var name = "kaylezy";
+console.log(name);
+
+var course = ["HTML", "CSS", "JavaScript"];
+for (course i = 0; i < 200; i++) {
+    if (i % 2 !== 0){ 
+        console.log(i);
+}
+};
