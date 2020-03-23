d664678eae64ca3e648b86682ebb2ae3c3e95226
Hassan Olakunle <kunlex_042003@yahoo.com>
2020-03-23 19:06:18 +0100

    script-file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var name = "kaylezy";
 console.log(name);
 
 var course = ["HTML", "CSS", "JavaScript"];
-for (course i = 0; i < 200; i++) {
+for (var i = 0; i < 200; i++) {
     if (i % 2 !== 0){ 
         console.log(i);
 }
