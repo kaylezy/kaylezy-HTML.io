8c2bdf4351b7cf2e9f39c15005c1224978b3f281
Hassan Olakunle <kunlex_042003@yahoo.com>
2020-04-19 12:31:04 +0100

    Js-task-3

---
+++ script3.js
@@ -0,0 +1,32 @@
+
+function divisible(number) {
+    let change = [];
+
+    for (let i = 1; i <= number; i++) {
+        let n = i;
+
+        if (i % 2 === 0) {   
+            n = "yu";
+        }
+        if (i % 3 === 0) {
+            if (n != i) {
+                n = n + "-gi"
+            }
+            else{
+                n = "gi";
+            }
+        }
+        if (i % 5 === 0) {
+            if (n != i) {
+                n = n + "-oh"
+            }
+            else{
+                n = "oh";
+            }
+        }
+        
+        change.push(n);
+    }
+    console.log(change)
+}
+divisible(10)
